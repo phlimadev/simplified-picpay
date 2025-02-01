73e9174b82f8c3ee5b906aee74b67c8b64d84823
phlimadev <devphlima@gmail.com>
2025-02-01 11:11:04 -0300

    creating user service

---
+++ src/main/java/com/example/simplified_picpay/services/UserService.java
@@ -0,0 +1,24 @@
+package com.example.simplified_picpay.services;
+
+import com.example.simplified_picpay.entities.User;
+import com.example.simplified_picpay.entities.UserType;
+import com.example.simplified_picpay.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User payer, BigDecimal amount) throws Exception {
+        if (payer.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Merchant cannot carry out transactions.");
+        }
+        if (payer.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Insufficient balance.");
+        }
+    }
+}
