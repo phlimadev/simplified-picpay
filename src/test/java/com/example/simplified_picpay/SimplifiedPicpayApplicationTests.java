b6567591fc1b666e75134c774b60e5688704ae29
phlimadev <devphlima@gmail.com>
2025-02-01 10:54:45 -0300

    initial config

---
+++ src/test/java/com/example/simplified_picpay/SimplifiedPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplified_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
