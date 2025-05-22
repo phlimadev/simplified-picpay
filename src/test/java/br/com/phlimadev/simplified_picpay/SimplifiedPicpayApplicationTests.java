bcf094f663aea1e8e20da2753d7794586b480ac9
phlimadev <devphlima@gmail.com>
2025-05-22 13:20:22 -0300

    first commit

---
+++ src/test/java/br/com/phlimadev/simplified_picpay/SimplifiedPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phlimadev.simplified_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
